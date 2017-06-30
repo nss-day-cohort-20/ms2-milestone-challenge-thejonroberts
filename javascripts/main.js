92cfb33f5ba607652c51c5b0223278ea59f8e9ed
thejonroberts <jon.roberts.8@gmail.com>
2017-06-30 14:18:35 -0500

    added DOM Manipulation module - DomManipulation.js.  Previous module housed there moved to 'DomInventory.js'

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,13 +1,2 @@
 CarLot.Inventory.loadInventory();
 
-
-
-
-
-// function buildRow() {
-
-// }
-
-// CarLot.Inventory.inventoryLoader.open("GET", '/data/inventory.json');
-// CarLot.Inventory.inventoryLoader.send();
-// buildCarBox();
