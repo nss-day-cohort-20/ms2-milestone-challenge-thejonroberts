9cca09e46d023445c1f35506a849161553bba53d
thejonroberts <jon.roberts.8@gmail.com>
2017-06-30 01:34:02 -0500

    request data and store in private var in Inventory module

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,20 +1,2 @@
-let productArray  = [];
-
-let inventoryRequest = new XMLHttpRequest();
-
-function errorHandler() {
-		console.log("An error occured while transfering data");
-}
-
-function dataHandler() {
-		let productArray = JSON.parse(event.target.responseText);
-				console.log('productArray', productArray);
-		// productArray.forEach(function(message) {
-		// });
-
-}
-	inventoryRequest.addEventListener("load", dataHandler);
-	inventoryRequest.addEventListener("error", errorHandler);
-	inventoryRequest.open("GET", '/data/inventory.json');
-	inventoryRequest.send();
+CarLot.Inventory.loadInventory();
 
