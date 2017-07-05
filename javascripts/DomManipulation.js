95c6873dd903c82a064786143ac5f2d3aebe0338
thejonroberts <jon.roberts.8@gmail.com>
2017-07-04 23:15:09 -0500

    listenter functions removed when conflicting events occur

--- javascripts/DomManipulation.js
+++ javascripts/DomManipulation.js
@@ -5,18 +5,14 @@ var CarLot = (function (globalScopeCarLot) {
 
 	let inputForm = document.getElementById("descriptionForm");
 	let inputTextField = document.getElementById("inputTextField");
-	let inputButton = document.getElementById("submitButton");
 	let currentCar = {};
 	let originalText = "";
 
-//test
-//
 	domChanges.targetCar = function() {
 		console.log('targetCar');
 		let carBoxes = document.querySelectorAll(".carBox");
 		//add listener to select a car to edit
 		carBoxes.forEach( function(carBox) {
-			// carBox.classList.remove("selectedBorder");
 			carBox.addEventListener( "click", function() {
 				currentCar = carBox;
 				currentCar.classList.add("selectedBorder");
@@ -28,21 +24,17 @@ var CarLot = (function (globalScopeCarLot) {
 	};
 
 	function enableFormInput() {
-		console.log('enableFormInput');
 		inputTextField.disabled = false;
-		inputButton.disabled = false;
 		inputTextField.value = currentCar.lastChild.innerText;
 		inputTextField.focus();
 		inputTextField.addEventListener( "keyup", changeDescription, false );
 	}
 
 	function disableFormInput() {
-		console.log('disableFormInput');
 		inputTextField.removeEventListener( "keyup", changeDescription, false );
-		inputTextField.blur(); //remove focus from input
-		inputTextField.value = "Select a car to edit description";
-		inputTextField.disabled = true; //disable input and submit button
-		inputButton.disabled = true;
+		inputTextField.blur();
+		inputTextField.value = "Select a car to edit description. Enter key submits changes.";
+		inputTextField.disabled = true;
 		document.removeEventListener( "focusout", cancelInputChange, false);
 	}
 
@@ -59,20 +51,19 @@ var CarLot = (function (globalScopeCarLot) {
 
 	function handleChangeSubmission() {
 		console.log('handleChangeSubmission');
-		document.removeEventListener( "focusout", cancelInputChange, false)
-		// CarLot.Inventory.changeDescription(car.id, inputTextField.value);
+		document.removeEventListener( "focusout", cancelInputChange, false);
+		//write changes to array QUESTION
+		CarLot.Inventory.changeDescription(currentCar.id, inputTextField.value);
 		removeBorders();
 		disableFormInput();
 		currentCar = {};
 	}
 
 	function watchForChangeSubmission() {
-		console.log('watchForChangeSubmission');
 		inputForm.addEventListener("submit", handleChangeSubmission, false);
 	}
 
 	function watchForInputCancel() {
-		console.log('watchForInputCancel');
 		originalText = currentCar.lastChild.innerText;
 		document.addEventListener( "focusout", cancelInputChange, false);
 	}
